6576bf09b63ba1f5ac2fad4fa6d767a6046f46d4
WrldEngine <kamscienceit123.kamran@gmail.com>
2025-11-18 03:01:31 +0500

    format: prettify the gpuOperators.cpp

--- jni/gpu/gpuOperators.cpp
+++ jni/gpu/gpuOperators.cpp
@@ -1,91 +1,164 @@
 #include <iostream>
 #include <vector>
+
 #include "gpuOperators.h"
 
 #define CL_TARGET_OPENCL_VERSION 220
 #include <CL/cl.h>
 
-#define assert_sz(vec1, vec2) { if (vec1.size() != vec2.size()) { std::cout << "Arrays not equal" << std::endl; exit(1); } } 
+#define assert_sz(vec1, vec2)                             \
+    {                                                     \
+        if (vec1.size() != vec2.size()) {                 \
+            std::cout << "Arrays not equal" << std::endl; \
+            exit(1);                                      \
+        }                                                 \
+    }
 
-std::string getErrorString(cl_int error)
-{
-    switch(error) {
+std::string getErrorString(cl_int error) {
+    switch (error) {
         // run-time and JIT compiler errors
-        case 0: return "CL_SUCCESS";
-        case -1: return "CL_DEVICE_NOT_FOUND";
-        case -2: return "CL_DEVICE_NOT_AVAILABLE";
-        case -3: return "CL_COMPILER_NOT_AVAILABLE";
-        case -4: return "CL_MEM_OBJECT_ALLOCATION_FAILURE";
-        case -5: return "CL_OUT_OF_RESOURCES";
-        case -6: return "CL_OUT_OF_HOST_MEMORY";
-        case -7: return "CL_PROFILING_INFO_NOT_AVAILABLE";
-        case -8: return "CL_MEM_COPY_OVERLAP";
-        case -9: return "CL_IMAGE_FORMAT_MISMATCH";
-        case -10: return "CL_IMAGE_FORMAT_NOT_SUPPORTED";
-        case -11: return "CL_BUILD_PROGRAM_FAILURE";
-        case -12: return "CL_MAP_FAILURE";
-        case -13: return "CL_MISALIGNED_SUB_BUFFER_OFFSET";
-        case -14: return "CL_EXEC_STATUS_ERROR_FOR_EVENTS_IN_WAIT_LIST";
-        case -15: return "CL_COMPILE_PROGRAM_FAILURE";
-        case -16: return "CL_LINKER_NOT_AVAILABLE";
-        case -17: return "CL_LINK_PROGRAM_FAILURE";
-        case -18: return "CL_DEVICE_PARTITION_FAILED";
-        case -19: return "CL_KERNEL_ARG_INFO_NOT_AVAILABLE";
+        case 0:
+            return "CL_SUCCESS";
+        case -1:
+            return "CL_DEVICE_NOT_FOUND";
+        case -2:
+            return "CL_DEVICE_NOT_AVAILABLE";
+        case -3:
+            return "CL_COMPILER_NOT_AVAILABLE";
+        case -4:
+            return "CL_MEM_OBJECT_ALLOCATION_FAILURE";
+        case -5:
+            return "CL_OUT_OF_RESOURCES";
+        case -6:
+            return "CL_OUT_OF_HOST_MEMORY";
+        case -7:
+            return "CL_PROFILING_INFO_NOT_AVAILABLE";
+        case -8:
+            return "CL_MEM_COPY_OVERLAP";
+        case -9:
+            return "CL_IMAGE_FORMAT_MISMATCH";
+        case -10:
+            return "CL_IMAGE_FORMAT_NOT_SUPPORTED";
+        case -11:
+            return "CL_BUILD_PROGRAM_FAILURE";
+        case -12:
+            return "CL_MAP_FAILURE";
+        case -13:
+            return "CL_MISALIGNED_SUB_BUFFER_OFFSET";
+        case -14:
+            return "CL_EXEC_STATUS_ERROR_FOR_EVENTS_IN_WAIT_LIST";
+        case -15:
+            return "CL_COMPILE_PROGRAM_FAILURE";
+        case -16:
+            return "CL_LINKER_NOT_AVAILABLE";
+        case -17:
+            return "CL_LINK_PROGRAM_FAILURE";
+        case -18:
+            return "CL_DEVICE_PARTITION_FAILED";
+        case -19:
+            return "CL_KERNEL_ARG_INFO_NOT_AVAILABLE";
 
         // compile-time errors
-        case -30: return "CL_INVALID_VALUE";
-        case -31: return "CL_INVALID_DEVICE_TYPE";
-        case -32: return "CL_INVALID_PLATFORM";
-        case -33: return "CL_INVALID_DEVICE";
-        case -34: return "CL_INVALID_CONTEXT";
-        case -35: return "CL_INVALID_QUEUE_PROPERTIES";
-        case -36: return "CL_INVALID_COMMAND_QUEUE";
-        case -37: return "CL_INVALID_HOST_PTR";
-        case -38: return "CL_INVALID_MEM_OBJECT";
-        case -39: return "CL_INVALID_IMAGE_FORMAT_DESCRIPTOR";
-        case -40: return "CL_INVALID_IMAGE_SIZE";
-        case -41: return "CL_INVALID_SAMPLER";
-        case -42: return "CL_INVALID_BINARY";
-        case -43: return "CL_INVALID_BUILD_OPTIONS";
-        case -44: return "CL_INVALID_PROGRAM";
-        case -45: return "CL_INVALID_PROGRAM_EXECUTABLE";
-        case -46: return "CL_INVALID_KERNEL_NAME";
-        case -47: return "CL_INVALID_KERNEL_DEFINITION";
-        case -48: return "CL_INVALID_KERNEL";
-        case -49: return "CL_INVALID_ARG_INDEX";
-        case -50: return "CL_INVALID_ARG_VALUE";
-        case -51: return "CL_INVALID_ARG_SIZE";
-        case -52: return "CL_INVALID_KERNEL_ARGS";
-        case -53: return "CL_INVALID_WORK_DIMENSION";
-        case -54: return "CL_INVALID_WORK_GROUP_SIZE";
-        case -55: return "CL_INVALID_WORK_ITEM_SIZE";
-        case -56: return "CL_INVALID_GLOBAL_OFFSET";
-        case -57: return "CL_INVALID_EVENT_WAIT_LIST";
-        case -58: return "CL_INVALID_EVENT";
-        case -59: return "CL_INVALID_OPERATION";
-        case -60: return "CL_INVALID_GL_OBJECT";
-        case -61: return "CL_INVALID_BUFFER_SIZE";
-        case -62: return "CL_INVALID_MIP_LEVEL";
-        case -63: return "CL_INVALID_GLOBAL_WORK_SIZE";
-        case -64: return "CL_INVALID_PROPERTY";
-        case -65: return "CL_INVALID_IMAGE_DESCRIPTOR";
-        case -66: return "CL_INVALID_COMPILER_OPTIONS";
-        case -67: return "CL_INVALID_LINKER_OPTIONS";
-        case -68: return "CL_INVALID_DEVICE_PARTITION_COUNT";
+        case -30:
+            return "CL_INVALID_VALUE";
+        case -31:
+            return "CL_INVALID_DEVICE_TYPE";
+        case -32:
+            return "CL_INVALID_PLATFORM";
+        case -33:
+            return "CL_INVALID_DEVICE";
+        case -34:
+            return "CL_INVALID_CONTEXT";
+        case -35:
+            return "CL_INVALID_QUEUE_PROPERTIES";
+        case -36:
+            return "CL_INVALID_COMMAND_QUEUE";
+        case -37:
+            return "CL_INVALID_HOST_PTR";
+        case -38:
+            return "CL_INVALID_MEM_OBJECT";
+        case -39:
+            return "CL_INVALID_IMAGE_FORMAT_DESCRIPTOR";
+        case -40:
+            return "CL_INVALID_IMAGE_SIZE";
+        case -41:
+            return "CL_INVALID_SAMPLER";
+        case -42:
+            return "CL_INVALID_BINARY";
+        case -43:
+            return "CL_INVALID_BUILD_OPTIONS";
+        case -44:
+            return "CL_INVALID_PROGRAM";
+        case -45:
+            return "CL_INVALID_PROGRAM_EXECUTABLE";
+        case -46:
+            return "CL_INVALID_KERNEL_NAME";
+        case -47:
+            return "CL_INVALID_KERNEL_DEFINITION";
+        case -48:
+            return "CL_INVALID_KERNEL";
+        case -49:
+            return "CL_INVALID_ARG_INDEX";
+        case -50:
+            return "CL_INVALID_ARG_VALUE";
+        case -51:
+            return "CL_INVALID_ARG_SIZE";
+        case -52:
+            return "CL_INVALID_KERNEL_ARGS";
+        case -53:
+            return "CL_INVALID_WORK_DIMENSION";
+        case -54:
+            return "CL_INVALID_WORK_GROUP_SIZE";
+        case -55:
+            return "CL_INVALID_WORK_ITEM_SIZE";
+        case -56:
+            return "CL_INVALID_GLOBAL_OFFSET";
+        case -57:
+            return "CL_INVALID_EVENT_WAIT_LIST";
+        case -58:
+            return "CL_INVALID_EVENT";
+        case -59:
+            return "CL_INVALID_OPERATION";
+        case -60:
+            return "CL_INVALID_GL_OBJECT";
+        case -61:
+            return "CL_INVALID_BUFFER_SIZE";
+        case -62:
+            return "CL_INVALID_MIP_LEVEL";
+        case -63:
+            return "CL_INVALID_GLOBAL_WORK_SIZE";
+        case -64:
+            return "CL_INVALID_PROPERTY";
+        case -65:
+            return "CL_INVALID_IMAGE_DESCRIPTOR";
+        case -66:
+            return "CL_INVALID_COMPILER_OPTIONS";
+        case -67:
+            return "CL_INVALID_LINKER_OPTIONS";
+        case -68:
+            return "CL_INVALID_DEVICE_PARTITION_COUNT";
 
         // extension errors
-        case -1000: return "CL_INVALID_GL_SHAREGROUP_REFERENCE_KHR";
-        case -1001: return "CL_PLATFORM_NOT_FOUND_KHR";
-        case -1002: return "CL_INVALID_D3D10_DEVICE_KHR";
-        case -1003: return "CL_INVALID_D3D10_RESOURCE_KHR";
-        case -1004: return "CL_D3D10_RESOURCE_ALREADY_ACQUIRED_KHR";
-        case -1005: return "CL_D3D10_RESOURCE_NOT_ACQUIRED_KHR";
-        default: return "Unknown OpenCL error";
+        case -1000:
+            return "CL_INVALID_GL_SHAREGROUP_REFERENCE_KHR";
+        case -1001:
+            return "CL_PLATFORM_NOT_FOUND_KHR";
+        case -1002:
+            return "CL_INVALID_D3D10_DEVICE_KHR";
+        case -1003:
+            return "CL_INVALID_D3D10_RESOURCE_KHR";
+        case -1004:
+            return "CL_D3D10_RESOURCE_ALREADY_ACQUIRED_KHR";
+        case -1005:
+            return "CL_D3D10_RESOURCE_NOT_ACQUIRED_KHR";
+        default:
+            return "Unknown OpenCL error";
     }
 }
 
-#define clErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
-inline void gpuAssert(cl_int error, const char *file, int line, bool abort = true) {
+#define clErrchk(ans) \
+    { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cl_int error, const char* file, int line, bool abort = true) {
     if (error != CL_SUCCESS) {
         fprintf(stderr, "GPUassert: %s %s %d\n", getErrorString(error).c_str(), file, line);
         if (abort) {
@@ -144,12 +217,8 @@ std::vector<int> OpenCLKernelsOperator::sumArrays(const std::vector<int>& a, con
     const char* source_c_str = source_str.c_str();
     const size_t source_size = source_str.size();
 
-    cl_program program = clCreateProgramWithSource(
-        this->context, 1,
-        (const char**)&source_c_str,
-        (const size_t*)&source_size,
-        &this->ret
-    );
+    cl_program program = clCreateProgramWithSource(this->context, 1, (const char**)&source_c_str,
+                                                   (const size_t*)&source_size, &this->ret);
     clErrchk(this->ret);
 
     cl_mem vec_a_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY, arr_sz * sizeof(int), NULL, &this->ret);
@@ -159,19 +228,11 @@ std::vector<int> OpenCLKernelsOperator::sumArrays(const std::vector<int>& a, con
     cl_mem res_mem_obj = clCreateBuffer(context, CL_MEM_WRITE_ONLY, arr_sz * sizeof(int), NULL, &this->ret);
     clErrchk(this->ret);
 
-    clErrchk(
-        clEnqueueWriteBuffer(
-            this->command_queue, vec_a_mem_obj, CL_TRUE, 0,
-            arr_sz*sizeof(int), a.data(), 0, NULL, NULL
-        )
-    );
+    clErrchk(clEnqueueWriteBuffer(this->command_queue, vec_a_mem_obj, CL_TRUE, 0, arr_sz * sizeof(int), a.data(), 0,
+                                  NULL, NULL));
 
-    clErrchk(
-        clEnqueueWriteBuffer(
-            this->command_queue, vec_b_mem_obj, CL_TRUE, 0,
-            arr_sz*sizeof(int), b.data(), 0, NULL, NULL
-        )
-    );
+    clErrchk(clEnqueueWriteBuffer(this->command_queue, vec_b_mem_obj, CL_TRUE, 0, arr_sz * sizeof(int), b.data(), 0,
+                                  NULL, NULL));
 
     clErrchk(clBuildProgram(program, 1, &this->dv_id, NULL, NULL, NULL));
 
@@ -190,16 +251,10 @@ std::vector<int> OpenCLKernelsOperator::sumArrays(const std::vector<int>& a, con
         local_size = max_local_size;
     }
 
-    clErrchk(clEnqueueNDRangeKernel(
-        command_queue, kernel, 1, NULL,
-        &global_size, &local_size,
-        0, NULL, NULL
-    ));
+    clErrchk(clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, &global_size, &local_size, 0, NULL, NULL));
 
-    clErrchk(clEnqueueReadBuffer(
-        this->command_queue, res_mem_obj, CL_TRUE, 0,
-        arr_sz * sizeof(float), result.data(), 0, NULL, NULL
-    ));
+    clErrchk(clEnqueueReadBuffer(this->command_queue, res_mem_obj, CL_TRUE, 0, arr_sz * sizeof(float), result.data(), 0,
+                                 NULL, NULL));
 
     ret = clFlush(command_queue);
     ret = clFinish(command_queue);
@@ -218,7 +273,7 @@ std::vector<int> OpenCLKernelsOperator::sumArrays(const std::vector<int>& a, con
 std::vector<int> OpenCLKernelsOperator::mulArrays(const std::vector<int>& a, const std::vector<int>& b) {
     assert_sz(a, b)
 
-    int arr_sz = a.size();
+        int arr_sz = a.size();
     std::vector<int> result(arr_sz);
 
     std::string source_str = R"==(
@@ -239,12 +294,8 @@ std::vector<int> OpenCLKernelsOperator::mulArrays(const std::vector<int>& a, con
     const char* source_c_str = source_str.c_str();
     const size_t source_size = source_str.size();
 
-    cl_program program = clCreateProgramWithSource(
-        this->context, 1,
-        (const char**)&source_c_str,
-        (const size_t*)&source_size,
-        &this->ret
-    );
+    cl_program program = clCreateProgramWithSource(this->context, 1, (const char**)&source_c_str,
+                                                   (const size_t*)&source_size, &this->ret);
     clErrchk(this->ret);
 
     cl_mem vec_a_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY, arr_sz * sizeof(int), NULL, &this->ret);
@@ -254,19 +305,11 @@ std::vector<int> OpenCLKernelsOperator::mulArrays(const std::vector<int>& a, con
     cl_mem res_mem_obj = clCreateBuffer(context, CL_MEM_WRITE_ONLY, arr_sz * sizeof(int), NULL, &this->ret);
     clErrchk(this->ret);
 
-    clErrchk(
-        clEnqueueWriteBuffer(
-            this->command_queue, vec_a_mem_obj, CL_TRUE, 0,
-            arr_sz*sizeof(int), a.data(), 0, NULL, NULL
-        )
-    );
+    clErrchk(clEnqueueWriteBuffer(this->command_queue, vec_a_mem_obj, CL_TRUE, 0, arr_sz * sizeof(int), a.data(), 0,
+                                  NULL, NULL));
 
-    clErrchk(
-        clEnqueueWriteBuffer(
-            this->command_queue, vec_b_mem_obj, CL_TRUE, 0,
-            arr_sz*sizeof(int), b.data(), 0, NULL, NULL
-        )
-    );
+    clErrchk(clEnqueueWriteBuffer(this->command_queue, vec_b_mem_obj, CL_TRUE, 0, arr_sz * sizeof(int), b.data(), 0,
+                                  NULL, NULL));
 
     clErrchk(clBuildProgram(program, 1, &this->dv_id, NULL, NULL, NULL));
 
@@ -285,16 +328,10 @@ std::vector<int> OpenCLKernelsOperator::mulArrays(const std::vector<int>& a, con
         local_size = max_local_size;
     }
 
-    clErrchk(clEnqueueNDRangeKernel(
-        command_queue, kernel, 1, NULL,
-        &global_size, &local_size,
-        0, NULL, NULL
-    ));
+    clErrchk(clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, &global_size, &local_size, 0, NULL, NULL));
 
-    clErrchk(clEnqueueReadBuffer(
-        this->command_queue, res_mem_obj, CL_TRUE, 0,
-        arr_sz * sizeof(float), result.data(), 0, NULL, NULL
-    ));
+    clErrchk(clEnqueueReadBuffer(this->command_queue, res_mem_obj, CL_TRUE, 0, arr_sz * sizeof(float), result.data(), 0,
+                                 NULL, NULL));
 
     ret = clFlush(command_queue);
     ret = clFinish(command_queue);
